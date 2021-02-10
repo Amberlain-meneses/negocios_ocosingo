eb89ce2a5621cf15f28954992411257392eb5fe4
Amberlain Meneses <amberlain98@gmail.com>
2021-02-09 23:12:51 -0600

    modificación login principal

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,6 +22,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/icheck-bootstrap/3.0.1/icheck-bootstrap.min.css"
           integrity="sha512-8vq2g5nHE062j3xor4XxPeZiPjmRDh6wlufQlfC6pdQ/9urJkU07NM0tEREeymP++NczacJ/Q59ul+/K2eYvcg=="
           crossorigin="anonymous"/>
+    
+    <link href="{{asset('css/main.css')}}" rel="stylesheet" />
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -31,79 +33,94 @@
     <![endif]-->
 
 </head>
-<body class="hold-transition login-page">
-<div class="login-box">
-    <div class="login-logo">
-        <a href="{{ url('/home') }}"><b>{{ config('app.name') }}</b></a>
-    </div>
+<body class="bg-ligth">
 
-    <!-- /.login-logo -->
-
-    <!-- /.login-box-body -->
-    <div class="card">
-        <div class="card-body login-card-body">
-            <p class="login-box-msg">Iniciar sesión <br>Negocios Ocosingo</p>
-
-            <form method="post" action="{{ url('/login') }}">
-                @csrf
-
-                <div class="input-group mb-3">
-                    <input type="email"
-                           name="email"
-                           value="{{ old('email') }}"
-                           placeholder="Correo electrónico"
-                           class="form-control @error('email') is-invalid @enderror">
-                    <div class="input-group-append">
-                        <div class="input-group-text"><span class="fas fa-envelope"></span></div>
+<section>
+    <div class="row">
+        <div class="col-lg-6">
+            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                
+                <div class="carousel-inner">
+                  <div class="carousel-item img-1 min-vh-100 active">
+                   
+                    <div class="carousel-caption d-none d-md-block">
+                   
+                      <h5 class="titleCarousel">Bienvenido</h5>
+                      <p class="">¿Todavía no tienes una cuenta? <br>has clic para registrarte</p>
+                      <a href="{{ route('register') }}" class="btn btn-outline-success">Registrar Nuevo Administrador</a>
                     </div>
-                    @error('email')
-                    <span class="error invalid-feedback">{{ $message }}</span>
-                    @enderror
+                   
+                  </div>
+                
+                 
                 </div>
 
-                <div class="input-group mb-3">
-                    <input type="password"
-                           name="password"
-                           placeholder="Contraseña"
-                           class="form-control @error('password') is-invalid @enderror">
-                    <div class="input-group-append">
-                        <div class="input-group-text">
-                            <span class="fas fa-lock"></span>
-                        </div>
-                    </div>
-                    @error('password')
-                    <span class="error invalid-feedback">{{ $message }}</span>
-                    @enderror
-
-                </div>
-
-                <div class="row">
-                    <div class="col-8">
-                        <div class="icheck-primary">
-                            <input type="checkbox" id="remember">
-                            <label for="remember">Recordar siempre</label>
-                        </div>
-                    </div>
-
-                    <div class="col-4">
-                        <button type="submit" class="btn btn-success btn-block">Iniciar</button>
-                    </div>
-
-                </div>
-            </form>
-
-            <p class="mb-1">
-                <a href="{{ route('password.request') }}">Olvidé mi contraseña</a>
-            </p>
-            <p class="mb-0">
-                <a href="{{ route('register') }}" class="text-center">Registrar nuevo administrador</a>
-            </p>
+              </div>
         </div>
-        <!-- /.login-card-body -->
+        <div class="col-lg-6 d-flex flex-column ">
+            <div class="px-lg-5 py-lg-4 p-4 ">
+                
+                <div class="input-group justify-content-center">
+                    <h2 class="titleForm ">Iniciar sesión</h2>
+                    <h1 class="tileNegocios mb-4">Negocios Ocosingo</h1>
+                </div>
+                
+                <form method="post" action="{{ url('/login') }}">
+                    @csrf
+    
+                    <div class="input-group mb-4 ">
+                        <input type="email"
+                               name="email"
+                               value="{{ old('email') }}"
+                               placeholder="Correo electrónico"
+                               class="form-control @error('email') is-invalid @enderror">
+                        <div class="input-group-append">
+                            <div class="input-group-text"><span class="fas fa-envelope"></span></div>
+                        </div>
+                        @error('email')
+                        <span class="error invalid-feedback">{{ $message }}</span>
+                        @enderror
+                    </div>
+    
+                    <div class="input-group mb-4">
+                        <input type="password"
+                               name="password"
+                               placeholder="Contraseña"
+                               class="form-control @error('password') is-invalid @enderror  w-50">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                        @error('password')
+                        <span class="error invalid-feedback">{{ $message }}</span>
+                        @enderror
+    
+                    </div>
+    
+                    <div class="row">
+                        
+                        <div class="input-group mb-4 justify-content-center">
+                            <button type="submit" class="btn  "  id="btnAccess">Iniciar</button>
+                        </div>
+                        <div class="col-8">
+                            <div class="icheck-primary">
+                                <input type="checkbox" id="remember">
+                                <label for="remember">Recordar siempre</label>
+                            </div>
+                            <p class="mb-4">
+                                <a id="olvidador" href="{{ route('password.request') }}" class="text-center">Olvidé mi contraseña</a>
+                            </p>
+                        </div>
+    
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
+</section>
 
-</div>
-<!-- /.login-box -->
+
 
 <!-- AdminLTE App -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.0.5/js/adminlte.min.js"
