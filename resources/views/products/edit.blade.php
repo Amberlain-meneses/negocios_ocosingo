5ef372cac32655a2dffab0f4c90bcd26412b9737
Amberlainm <59625025+Amberlain-meneses@users.noreply.github.com>
2021-02-13 16:17:36 -0600

    Cambio de vistas de productos

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -5,7 +5,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-12">
-                    <h1>Edit Product</h1>
+                    <h1>Actualizar datos de productos</h1>
                 </div>
             </div>
         </div>
@@ -26,8 +26,8 @@
             </div>
 
             <div class="card-footer">
-                {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-                <a href="{{ route('products.index') }}" class="btn btn-default">Cancel</a>
+                {!! Form::submit('Guardar', ['class' => 'btn btn-success']) !!}
+                <a href="{{ route('products.index') }}" class="btn btn-outline-success">Cancelar</a>
             </div>
 
            {!! Form::close() !!}
