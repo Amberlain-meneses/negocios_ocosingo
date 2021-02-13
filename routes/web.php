7fe16f919e2ed8d7ba744b649f4b5a5c8fa1d632
unknown <jose.santizal@laselva.edu.mx>
2021-02-13 17:19:57 -0600

    archivo de rutas

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,50 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+//Rutas para productos
+Route::group(['middleware' => ['permission:products.index']], function () {
+    Route::get('products', 'ProductController@index')->name('products.index');
+});
 
-Route::resource('businesses', 'BusinessController');
+Route::group(['middleware' => ['permission:products.create']], function () {
+    Route::get('products/create', 'ProductController@create')->name('products.create');
+    Route::post('products/store', 'ProductController@store')->name('products.store');
+});
 
-Route::resource('products', 'ProductController');
+Route::group(['middleware' => ['permission:products.edit']], function () {
+    Route::get('products/{product}/edit', 'ProductController@edit')->name('products.edit');
+    Route::patch('products/{product}', 'ProductController@update')->name('products.update');
+});
+
+Route::group(['middleware' => ['permission:products.show']], function () {
+    Route::get('products/{product}', 'ProductController@show')->name('products.show');
+});
+
+Route::group(['middleware' => ['permission:products.destroy']], function () {
+    Route::delete('products/{product}', 'ProductController@destroy')->name('products.destroy');
+});
+
+
+
+//Rutas para negocios
+Route::group(['middleware' => ['permission:businesses.index']], function () {
+    Route::get('businesses', 'BusinessController@index')->name('businesses.index');
+});
+
+Route::group(['middleware' => ['permission:businesses.create']], function () {
+    Route::get('businesses/create', 'BusinessController@create')->name('businesses.create');
+    Route::post('businesses/store', 'BusinessController@store')->name('businesses.store');
+});
+
+Route::group(['middleware' => ['permission:businesses.edit']], function () {
+    Route::get('businesses/{business}/edit', 'BusinessController@edit')->name('businesses.edit');
+    Route::patch('businesses/{business}', 'BusinessController@update')->name('businesses.update');
+});
+
+Route::group(['middleware' => ['permission:businesses.show']], function () {
+    Route::get('businesses/{business}', 'BusinessController@show')->name('businesses.show');
+});
+
+Route::group(['middleware' => ['permission:businesses.destroy']], function () {
+    Route::delete('businesses/{business}', 'BusinessController@destroy')->name('businesses.destroy');
+});
